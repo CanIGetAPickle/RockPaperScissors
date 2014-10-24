32da136a7ec411a3362718e3bc8a6b241e356864
Dan Santos <danielrobertsantos@gmail.com>
2014-10-24 03:30:04 -0400

    Fixed indentation.

--- rockpaperscissors3.rb
+++ rockpaperscissors3.rb
@@ -2,36 +2,36 @@ CHOICES = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
 puts "Welcome to Rock, Paper, Scissors!"
 
 def display_winning_message(winning_choice)
-	case winning_choice
-	when 'p'
-		puts "Paper covers Rock!"
-	when 'r'
-		puts "Rock damages Scissors!"
-	when 's'
-		puts "Scissors cuts Paper!"
-	end
+  case winning_choice
+  when 'p'
+    puts "Paper covers Rock!"
+  when 'r'
+    puts "Rock damages Scissors!"
+  when 's'
+    puts "Scissors cuts Paper!"
+  end
 end
 
 loop do 
-	begin
-		puts "Pick one: (R/P/S):"
-		player_choice = gets.chomp.downcase
-	end until CHOICES.keys.include?(player_choice)
+  begin
+    puts "Pick one: (R/P/S):"
+    player_choice = gets.chomp.downcase
+  end until CHOICES.keys.include?(player_choice)
 
-	computer_choice = CHOICES.keys.sample
+  computer_choice = CHOICES.keys.sample
 
-	if player_choice == computer_choice
-		puts "It's a tie!"
-	elsif (player_choice == 'p' && computer_choice == 'r') || (player_choice == 'r' && computer_choice == 's') || (player_choice == 's' && computer_choice == 'p')
-		display_winning_message(player_choice)
-		puts "You won!"
-	else
-		display_winning_message(computer_choice)
-		puts "Computer won!"
-	end
+  if player_choice == computer_choice
+    puts "It's a tie!"
+  elsif (player_choice == 'p' && computer_choice == 'r') || (player_choice == 'r' && computer_choice == 's') || (player_choice == 's' && computer_choice == 'p')
+    display_winning_message(player_choice)
+    puts "You won!"
+  else
+    display_winning_message(computer_choice)
+    puts "Computer won!"
+  end
 
-	puts "Play again? (y/n)"
-	break if gets.chomp.downcase != 'y'
+  puts "Play again? (y/n)"
+  break if gets.chomp.downcase != 'y'
 
 end
 
